95af5d300b6e9d9aee27dabb41dbf116a48fc4ab
ahbarnett <alex.h.barnett@gmail.com>
2021-11-01 18:39:24 -0400

    fixed outdated nufft_opts docs

--- src/FINUFFT.jl
+++ src/FINUFFT.jl
@@ -28,35 +28,56 @@ const libfinufft = finufft_jll.libfinufft
 
 const BIGINT = Int64 # defined in include/dataTypes.h
 
+# our allowed real array types...
 const finufftReal = Union{Float64,Float32}
 
 ## FINUFFT opts struct from include/nufft_opts.h
 """
     mutable struct nufft_opts    
+        modeord            :: Cint
+        chkbnds            :: Cint
         debug              :: Cint
         spread_debug       :: Cint
+        showwarn           :: Cint
+        nthreads           :: Cint
+        fftw               :: Cint
         spread_sort        :: Cint
         spread_kerevalmeth :: Cint
         spread_kerpad      :: Cint
-        chkbnds            :: Cint
-        fftw               :: Cint
-        modeord            :: Cint
         upsampfac          :: Cdouble
         spread_thread      :: Cint
         maxbatchsize       :: Cint
-        nthreads           :: Cint
-        showwarn           :: Cint
+        spread_nthr_atomic :: Cint
+        spread_max_sp_size :: Cint
     end
 
 Options struct passed to the FINUFFT library.
 
 # Fields
 
+Summary only; see FINUFFT documentation for full descriptions.
+
+    modeord :: Cint
+0: CMCL-style increasing mode ordering (neg to pos), or\\
+1: FFT-style mode ordering (affects type-1,2 only)
+
+    chkbnds :: Cint
+0: don't check if input NU pts in [-3pi,3pi], 1: do
+
     debug :: Cint
 0: silent, 1: text basic timing output
 
     spread_debug :: Cint
 passed to spread_opts, 0 (no text) 1 (some) or 2 (lots)
+
+    showwarn :: Cint
+Whether to print warnings to stderr. 0: silent, 1: print warnings
+    
+    nthreads :: Cint
+How many threads FINUFFT should use, or 0 (use max available in OMP)
+
+    fftw :: Cint
+0:`FFTW_ESTIMATE`, or 1:`FFTW_MEASURE` (slow plan but faster FFTs)
 
     spread_sort :: Cint
 passed to spread_opts, 0 (don't sort) 1 (do) or 2 (heuristic)
@@ -67,33 +88,24 @@ passed to spread_opts, 0: exp(sqrt()), 1: Horner ppval (faster)
     spread_kerpad :: Cint
 passed to spread_opts, 0: don't pad to mult of 4, 1: do
 
-    chkbnds :: Cint
-0: don't check if input NU pts in [-3pi,3pi], 1: do
-
-    fftw :: Cint
-0:`FFTW_ESTIMATE`, or 1:`FFTW_MEASURE` (slow plan but faster)
-
-    modeord :: Cint
-0: CMCL-style increasing mode ordering (neg to pos), or\\
-1: FFT-style mode ordering (affects type-1,2 only)
-
     upsampfac :: Cdouble
-upsampling ratio sigma, either 2.0 (standard) or 1.25 (small FFT)
+upsampling ratio sigma: 2.0 (standard), or 1.25 (small FFT), or\\
+0.0 (auto).
 
     spread_thread :: Cint
-for ntrans>1 only.\\
-0:auto,\\
+(for ntrans>1 only)\\
+0: auto choice,\\
 1: sequential multithreaded,\\
-2: parallel singlethreaded (Melody),\\
-3: nested multithreaded (Andrea).
+2: parallel singlethreaded spread.
 
     maxbatchsize :: Cint
-// for ntrans>1 only. max blocking size for vectorized, 0 for auto-set
+(for ntrans>1 only). max blocking size for vectorized, 0 for auto-set
 
     spread_nthr_atomic :: Cint
 if >=0, threads above which spreader OMP critical goes atomic
+
     spread_max_sp_size :: Cint
-if >0, overrides spreader (dir=1) max subproblem size
+if >0, overrides spreader (dir=1 only) max subproblem size
 """
 mutable struct nufft_opts{T}    
     modeord            :: Cint
@@ -106,15 +118,18 @@ mutable struct nufft_opts{T}
     spread_sort        :: Cint
     spread_kerevalmeth :: Cint
     spread_kerpad      :: Cint
-    upsampfac          :: Cdouble
+    upsampfac          :: Cdouble        # regardless of T
     spread_thread      :: Cint
     maxbatchsize       :: Cint
     spread_nthr_atomic :: Cint
     spread_max_sp_size :: Cint
     nufft_opts{T}() where T <: finufftReal = new{T}()
 end
+# The above must match include/nufft_opts.h in FINUFFT.
+# Note that nufft_opts happens to be the same for Float32 vs Float64 types.
+# This is as in FINUFFT. It may not always hold in the future.
 
-const nufft_c_opts = nufft_opts # backward compability
+const nufft_c_opts = nufft_opts        # for backward compatibility - remove?
 
 """
     finufft_default_opts()
