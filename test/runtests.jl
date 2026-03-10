52398fffdb77835ad346bd3b02c6246cb6639423
Jonas Krimmer <30567171+jkrimmer@users.noreply.github.com>
2026-03-10 15:47:25 +0100

    feat: test thread-safety (#65)
    * feat: test thread-safety
    
    * ci: also run on arm64
    
    This way, we should be able to reproduce the failing threading tests
    
    * fix: arm64 only for Mac on julia 1.10
    
    * fix: missing quotes
    
    * try adding multiple threads
    
    * feat: generalize CI for linux on arm64
    
    * simplify test script
    
    * update setup-julia to v2 - arm64 fix?
    
    * revert arm64 changes
    
    * fix: update threading test to account for fft calls
    
    * fix: add FFTW to test targets in Project.toml
    
    * chore: improve test description
    
    ---------
    
    Co-authored-by: Ludvig af Klinteberg <ludvig.af.klinteberg@mdu.se>

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,6 +3,7 @@ using Test
 @testset "FINUFFT" begin
     include("test_error_handler.jl")
     include("test_nufft.jl")
+    include("test_thread_safety.jl")
 end
 @testset "cuFINUFFT" begin
     include("test_cuda.jl")
